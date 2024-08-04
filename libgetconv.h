abdce5783c775b013c5683fe6521c0c5f531ab06
Update56 <heupdate56@gmail.com>
2024-08-04 23:24:42 +0700

    Lib compiled

--- libgetconv.h
+++ libgetconv.h
@@ -74,7 +74,7 @@ typedef struct { void *data; GoInt len; GoInt cap; } GoSlice;
 extern "C" {
 #endif
 
-extern __declspec(dllexport) char* Getconv();
+extern __declspec(dllexport) void Getconv(float* rates);
 
 #ifdef __cplusplus
 }
